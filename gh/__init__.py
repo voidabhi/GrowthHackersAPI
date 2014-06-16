7a811b323755be0cd903fd4c075716fb26b50d0b
Abhijeet <void.aby@gmail.com>
2014-06-16 22:17:12 +0530

    added main package and tests

---
+++ gh/__init__.py
@@ -0,0 +1,11 @@
+"""
+Python API for Growth Hackers.
+
+@author Abhijeet Mohan
+@email abhijeetshibu@gmail.com
+"""
+
+__title__ = 'Growth Hackers'
+__author__ = 'Abhijeet Mohan'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2014 Abhijeet Mohan'
